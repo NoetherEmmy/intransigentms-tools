eb14c1d90565674e03febfaa7984a4e228922e4f
Emmy Noether <intransigentms@gmail.com>
2016-10-16 04:34:35 -0700

    Fixed cash shop glitch
    Fixed a glitch due to, for some god forsaken reason, `#L-2##l` in cm.sendSimple() messages sending players to the cash shop(?!) upon selecting it.

--- Dialogue-js/Dialogue.js
+++ Dialogue-js/Dialogue.js
@@ -20,28 +20,29 @@ var Dialogue = (function() {
                     return lastPrompt;
                 }
 
-                if (nodeIndex.length === 0 && selection < 0) return exit();
-
                 var i;
                 if (selection !== undefined) {
                     if (selection == -1) {
                         if (opts.endchat == "continue") {
                             selection = 0;
                         } else if (opts.endchat == "back") {
-                            selection = -2;
+                            selection = -3;
                         } else if (opts.endchat == "stay") {
                             selection = -1;
                         } else {
                             return exit();
                         }
                     }
+
+                    if (nodeIndex.length === 0 && selection < -1) return exit();
+
                     if (selection >= 0) {
                         if (!node.choices[selection]) return exit();
                         node = node.choices[selection][1];
                         nodeIndex.push(selection);
                     } else if (selection != -1) {
-                        for (i = -1; i > selection; --i) {
-                            if (!nodeIndex.pop()) return exit();
+                        for (i = -2; i > selection; --i) {
+                            if (nodeIndex.pop() === undefined) return exit();
                         }
                         node = convo;
                         for (i = 0; i < nodeIndex.length; ++i) {
@@ -51,19 +52,18 @@ var Dialogue = (function() {
                 }
 
                 if (!node || !node.prompt) return exit();
+                lastPrompt = node.prompt;
                 if (!node.choices) {
-                    lastPrompt = node.prompt;
                     cm.sendOk(lastPrompt);
                     return exit();
                 }
 
-                lastPrompt = node.prompt;
-                var message = node.prompt + "\r\n\r\n";
+                var message = lastPrompt + "\r\n\r\n";
                 var choices = node.choices;
                 for (i = 0; i < choices.length; ++i) {
                     var choice = choices[i];
                     if (choice[1].move !== undefined) {
-                        message += "#L" + (choice[1].move > 0 ? i : choice[1].move - 1) + "#" + choice[0] + "#l\r\n";
+                        message += "#L" + (choice[1].move > 0 ? i : (choice[1].move == 0 ? -1 : choice[1].move - 2)) + "#" + choice[0] + "#l\r\n";
                     } else {
                         message += "#L" + i + "#" + (Array.isArray(choice) ? choice[0] : choice) + "#l\r\n";
                     }
